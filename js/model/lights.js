34b25f6baa06d015fd1b096a3f3494e532282b2f
lavrowlad <lavrowlad@gmail.com>
2016-10-23 23:22:30 +0300

    Code refactoring (1)
    Разбиение стилей на файлы.
    Разбиение js-кода на модули.
    Новый список подключаемых файлов в index.html.

--- js/model/lights.js
+++ js/model/lights.js
@@ -1,14 +1,15 @@
 class Light {
-    constructor(_id, _description){
+    constructor(_id, _type, _description){
         this.id = _id;
-        this.type = "lighting";
+        this.type = _ype;
         this.description = _description;
     }
     
     get DOMelement(){
         var element = document.createElement("div");
         element.setAttribute("class", this.type);
-        /*element.innerHTML = this.description;*/
+        
+        
         
         return element;
     }
